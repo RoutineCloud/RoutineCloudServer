0253d5af623ca4a9fb61c51593cca32ea535bcf1
Paul Kruse <paul.kruse@posteo.de>
2026-02-21 20:47:54 +0100

    Refactor OAuth2 endpoints and integrate updated forms in frontend
    - Enhanced backend OAuth2 endpoints with `operation_id` fields and updated response models.
    - Added `/authorize` endpoint with support for logged-in user authorization.
    - Replaced deprecated frontend methods (`deviceVerifyApiOauthDeviceVerifyPost`, `authorizeJsonApiOauthAuthJsonPost`) with streamlined versions (`oauthDeviceVerify`, `oauthAuthorize`).

--- frontend/src/views/OAuthAuthorize.vue
+++ frontend/src/views/OAuthAuthorize.vue
@@ -139,9 +139,9 @@ const authorizeAccount = async () => {
   
   try {
 
-    const {data, error} = await Oauth2.authorizeJsonApiOauthAuthJsonPost(
+    const {data, error} = await Oauth2.oauthAuthorize(
         {
-          query: {
+          body: {
             client_id:clientId,
             redirect_uri:redirectUri,
             response_type:responseType,
